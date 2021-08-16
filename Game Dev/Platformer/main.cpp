da659745194fb473f5d0d92ebbf962432a00b393
TriangleDevv <chandu.lanka13@gmail.com>
2021-08-16 19:43:01 +0530

    🏹getting player input

--- Game Dev/Platformer/main.cpp
+++ Game Dev/Platformer/main.cpp
@@ -1,9 +1,19 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
+using namespace std;
 
 int windowWidth = 400;
 int windowHeight = 300;
+
+class playerClass {
+    public:
+        playerClass()
+        {
+
+        }
+};
 
 int main()
 {
@@ -12,6 +22,7 @@ int main()
 
     // variables
     bool up, down, left, right = false;
+    playerClass player;
 
 	// Start the game loop
     while (app.isOpen())
@@ -24,6 +35,19 @@ int main()
             if (event.type == Event::Closed)
                 app.close();
         }
+
+        if(Keyboard::isKeyPressed(Keyboard::Right)) right = true;
+        else if(Keyboard::isKeyPressed(Keyboard::Left)) left = true;
+        else if(Keyboard::isKeyPressed(Keyboard::Up)) up = true;
+        else if(Keyboard::isKeyPressed(Keyboard::Down)) down = true;
+        else {
+            up = false;
+            down = false;
+            left = false;
+            right = false;
+        }
+
+        cout << down << endl;
 
         // Clear screen
         app.clear();
