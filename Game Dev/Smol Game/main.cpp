5711a9ad3461c44dc66eb78bb007e0b240cc98fe
TriangleDevv <chandu.lanka13@gmail.com>
2021-08-18 16:38:29 +0530

    ⛹🏻‍♂️got the bouncing mechanic done

--- Game Dev/Smol Game/main.cpp
+++ Game Dev/Smol Game/main.cpp
@@ -4,35 +4,87 @@
 using namespace std;
 using namespace sf;
 
-int windowWidth = 512;
-int windowHeight = 512;
+int windowWidth = 600;
+int windowHeight = 400;
+
+float x = 0;
+float y = 0;
+float momentum = 0.f;
+float width = 50.f;
+float height = 50.f;
+bool movingRight = false;
+bool movingLeft = false;
 
 int main()
 {
     RenderWindow window(VideoMode(windowWidth, windowHeight), "Smol Game", Style::Close | Style::Titlebar);
-
-    while(window.isOpen())
+    while (window.isOpen())
     {
-        Event listener;
-        while (window.pollEvent(listener))
+        Event event;
+        while (window.pollEvent(event))
         {
-            switch (listener.type)
+            switch(event.type)
             {
                 case Event::Closed:
                     window.close();
                     break;
-                case Event::Resized:
-                    cout << "New Window Width: " << listener.size.width << "New Window Height: " << listener.size.height << endl;
-                    break;
-                case Event::TextEntered:
-                    if (listener.text.unicode < 128)
+                case Event::KeyPressed:
+                    if (event.key.code == Keyboard::Right)
                     {
-                        printf("%c", listener.text.unicode);
+                        movingRight = true;
                     }
-
+                    else if (event.key.code == Keyboard::Left)
+                    {
+                        movingLeft = true;
+                    }
+                    break;
+                case Event::KeyReleased:
+                    if (event.key.code == Keyboard::Right)
+                    {
+                        movingRight = false;
+                    }
+                    else if (event.key.code == Keyboard::Left)
+                    {
+                        movingLeft = false;
+                    }
+                    break;
             }
         }
-    }
 
-    return 0;
+        // bouncing feature
+        if (y > windowHeight - height)
+        {
+            momentum = -momentum;
+        }
+        else {
+            momentum += 0.2;
+        }
+        y += momentum;
+
+        // movement
+        if (movingRight)
+        {
+            x += 5;
+        }
+        if (movingLeft)
+        {
+            x -= 5;
+        }
+
+        // clear window
+        window.clear(Color::Black);
+
+        // draw entities here
+
+        RectangleShape rect(Vector2f(width, height));
+        rect.setFillColor(Color::Red);
+        rect.setPosition(x, y);
+        window.draw(rect);
+
+        // display stuff on the screen
+        window.display();
+
+        // fps
+        window.setFramerateLimit(60);
+    }
 }
